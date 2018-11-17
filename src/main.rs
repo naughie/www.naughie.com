fa7f483184abbd9ed27ecff1e30d422996db9750
Naughie <zomacchi@gmail.com>
2018-11-17 16:34:44 +0900

    Serve static files for advent calendar

--- src/main.rs
+++ src/main.rs
@@ -2,11 +2,15 @@
 
 #[macro_use] extern crate rocket;
 
+use rocket_contrib::serve::StaticFiles;
+
 #[get("/")]
 fn hello() -> &'static str {
     "Hello, World!テスト😀ほげほげ\n"
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello]).launch();
+    rocket::ignite().mount("/", routes![hello])
+        .mount("/advent", StaticFiles::from("public/articles/advent"))
+        .launch();
 }
