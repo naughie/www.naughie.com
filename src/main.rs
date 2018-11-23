bc5c03b55dcfe72f124485868a8badc3b010ad77
Naughie <zomacchi@gmail.com>
2018-11-23 21:59:00 +0900

    Add index.html to root

--- src/main.rs
+++ src/main.rs
@@ -2,15 +2,19 @@
 
 #[macro_use] extern crate rocket;
 
+use rocket::response::NamedFile;
+use rocket::response::status::NotFound;
 use rocket_contrib::serve::StaticFiles;
 
 #[get("/")]
-fn hello() -> &'static str {
-    "Hello, World!テスト😀ほげほげ\n"
+fn root() -> Result<NamedFile, NotFound<String>> {
+    NamedFile::open("public/index.html")
+        .map_err(|_| NotFound("Not Found!😭\n".to_string()))
 }
 
 fn main() {
-    rocket::ignite().mount("/", routes![hello])
+    rocket::ignite()
+        .mount("/", routes![root])
         .mount("/advent", StaticFiles::from("public/articles/advent"))
         .launch();
 }
